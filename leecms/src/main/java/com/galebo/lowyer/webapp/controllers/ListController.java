003e354b870873ecdca3af8d1b7e20c06c6383cb
galebo <galebo@163.com>
2014-03-14 14:45:24 +0800

    compress BeanController to ListController

---
+++ leecms/src/main/java/com/galebo/lowyer/webapp/controllers/ListController.java
@@ -0,0 +1,96 @@
+package com.galebo.lowyer.webapp.controllers;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.galebo.lowyer.Constants;
+import com.galebo.lowyer.model.CfgDefine;
+import com.galebo.lowyer.model.ConContent;
+import com.galebo.lowyer.model.ProCategory;
+import com.galebo.lowyer.model.ProProduct;
+import com.galebo.lowyer.model.Template;
+import com.galebo.lowyer.model.TemplatePage;
+import com.galebo.lowyer.model.User;
+import com.galebo.lowyer.webapp.controller.BaseController;
+import com.galebo.lowyer.webapp.controller.BaseFormController;
+
+@Controller
+public class ListController extends BaseController {
+
+	@RequestMapping("/conContents*")
+	public ModelAndView conContents(@RequestParam(required = false, value = "q") String query, HttpServletRequest request) throws Exception {
+		return new ModelAndView().addObject(commonService.getQueryDao().getConContentsByUserId(getCurrentUser(request)));
+	}
+
+	@RequestMapping(value = "/conUploadDatas*", method = RequestMethod.GET)
+	public ModelAndView conUploadDatas(@RequestParam(required = false, value = "q") String query, HttpServletRequest request) throws Exception {
+		return new ModelAndView().addObject(commonService.getQueryDao().getConUploadDatasByUserId(getCurrentUser(request)));
+	}
+
+	@RequestMapping(value = "/cfgGroups*", method = RequestMethod.GET)
+	public ModelAndView cfgGroups(@RequestParam(required = false, value = "q") String query, @RequestParam("type") String type,
+			HttpServletRequest request) throws Exception {
+		ModelAndView addObject = new ModelAndView().addObject(commonService.getQueryDao().getGroup(type, getCurrentUser(request)));
+		addObject.addObject("type", type);
+		return addObject;
+	}
+
+	@RequestMapping(value = "/defineInstances*", method = RequestMethod.GET)
+	public ModelAndView defineInstances(@RequestParam(required = false, value = "q") String query, HttpServletRequest request) throws Exception {
+		return new ModelAndView().addObject(userDataCreator.getSiteDefineWithName(BaseFormController.getCurrentUser(request)));
+	}
+
+	@RequestMapping(value = "/conLinks/{type}", method = RequestMethod.GET)
+	public ModelAndView conLinks(@PathVariable String type, HttpServletRequest request) throws Exception {
+		ModelAndView addObject = new ModelAndView("conLinks").addObject(commonService.getQueryDao().getLinks(type, getCurrentUser(request)));
+		addObject.addObject("type", type);
+		return addObject;
+	}
+
+	@RequestMapping(value = "/defines*", method = RequestMethod.GET)
+	public ModelAndView defines(@RequestParam(required = false, value = "q") String query) throws Exception {
+		return new ModelAndView().addObject(commonService.search(query, CfgDefine.class));
+	}
+
+	@RequestMapping(value = "/proCategories*", method = RequestMethod.GET)
+	public ModelAndView proCategories(@RequestParam(required = false, value = "q") String query) throws Exception {
+		return new ModelAndView().addObject(commonService.search(query, ProCategory.class));
+	}
+
+	@RequestMapping(value = "/proProducts*", method = RequestMethod.GET)
+	public ModelAndView proProducts(@RequestParam(required = false, value = "q") String query) throws Exception {
+		return new ModelAndView().addObject(commonService.search(query, ProProduct.class));
+	}
+
+	@RequestMapping(value = "/templates*", method = RequestMethod.GET)
+	public ModelAndView templates(@RequestParam(required = false, value = "q") String query) throws Exception {
+		return new ModelAndView().addObject(commonService.search(query, Template.class));
+	}
+
+	@RequestMapping(value = "/templatePages*", method = RequestMethod.GET)
+	public ModelAndView templatePages(HttpServletRequest request) throws Exception {
+		return new ModelAndView().addObject(commonService.search("", TemplatePage.class));
+	}
+
+	@RequestMapping(value = "/admin/users*", method = RequestMethod.GET)
+	public ModelAndView users(@RequestParam(required = false, value = "q") String query) throws Exception {
+		return new ModelAndView("admin/userList", Constants.USER_LIST, commonService.search(query, User.class));
+	}
+
+	@RequestMapping("/export")
+	public ModelAndView handleRequest() {
+		List<ConContent> all = commonService.getQueryDao().getConContentDao().getAll();
+		for (int i = 0; i < all.size(); i++) {
+
+		}
+		return null;
+	}
+}
