24ca58c1d2e416cb96238113e33acef8af770ced
galebo <galebo@163.com>
2014-03-20 09:54:49 +0800

    增加bean的生成

--- leecms/src/main/java/com/galebo/lowyer/services/mock/BeanCreator.java
+++ leecms/src/main/java/com/galebo/lowyer/services/mock/BeanCreator.java
@@ -1,12 +1,19 @@
 package com.galebo.lowyer.services.mock;
 
+import java.io.FileInputStream;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.io.IOUtils;
+
+import com.galebo.lowyer.bean.Common;
+import com.galebo.lowyer.bean.Detail;
 import com.galebo.lowyer.bean.Index;
 import com.galebo.lowyer.bean.Item;
 import com.galebo.lowyer.bean.Menu;
 import com.galebo.lowyer.bean.common.UrlNameAuthor;
+import com.galebo.lowyer.bean.common.UrlNameAuthorHtml;
 
 public class BeanCreator {
 
@@ -38,6 +45,27 @@ public class BeanCreator {
 		System.out.println(menus.size());
 		return index;
 	}
+	
+	public Common getCommon(){
+		Common common = new Common();
+		common.setMenuList(getMenu(false));
+		Item defaultItem = getItem();
+		common.setDefaultItem(defaultItem);
+		return common;
+	}
+	public Item getItem() {
+		Item defaultItem = new Item();
+		ArrayList<UrlNameAuthor> contents = new ArrayList<UrlNameAuthor>();
+		for (int i = 0; i < 10; i++) {
+			UrlNameAuthor e = new UrlNameAuthor("", "执行预案威力大 快速执结效果好");
+			e.setUpdateTime(new Date());
+			contents.add(e);
+		}
+		defaultItem.setContents(contents);
+		defaultItem.setName("刑罚分类");
+		defaultItem.setUrl("#");
+		return defaultItem;
+	}
 	private List<Menu> getMenu(boolean addSon) {
 		List<Menu> menus=new ArrayList<Menu>();
 		for (int i = 0; i < 5; i++) {
@@ -49,5 +77,40 @@ public class BeanCreator {
 			menus.add(menu);
 		}
 		return menus;
+	}
+	public Detail getDetail() {
+		Detail detail=new Detail();
+		UrlNameAuthorHtml content = getContent("1");
+		detail.setContent(content);
+		content.setPre(getContent("2") );
+		content.setNext(getContent("3"));
+		content.setUpdateTime(new Date());
+		
+		detail.setItem(getItem());
+		
+		detail.setParentItems(getItemSimpleName());
+		return detail;
+	}
+
+	private List<Item> getItemSimpleName() {
+		ArrayList<Item> parentItems = new ArrayList<Item>();
+		for (int i = 0; i < 10; i++) {
+			Item e = new Item();
+			e.setName("刑罚分类"+i);
+			parentItems.add(e);
+		}
+		return parentItems;
+	}
+	private UrlNameAuthorHtml getContent(String ddd) {
+		UrlNameAuthorHtml content = new UrlNameAuthorHtml();
+		content.setAuthor("作者");
+		try {
+			content.setHtml(IOUtils.toString(new FileInputStream("E:/workspace/git/LeeCMS/leecms/src/test/java/com/galebo/lowyer/content.txt")));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		content.setUrl("#");
+		content.setName(" 加强侦查监督能力建设"+ddd);
+		return content;
 	}
 }
